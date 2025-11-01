fe4f8ac3695a9b05292c0a92f1ce4c54e4031f2d
nada969 <nadamousa969@gmail.com>
2025-11-01 16:22:39 +0200

    sec approach with HashMap

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -36,35 +36,61 @@ public class App
             MongoCollection<Document> collection = db.getCollection(c);
 
             for(Document doc:collection.find()){
-                List<String> columns = new ArrayList<String>();
-                List<String> values =  new ArrayList<String>();
-
-//                if(c.equals("orders")){
+// //               ArrayList
+//                List<String> columns = new ArrayList<String>();
+//                List<String> values =  new ArrayList<String>();
+// //              HashMap
+                HashMap<String,Object> docData = new HashMap<>();
 
                     for(String key:doc.keySet()) {
                         Object value = doc.get(key);
 
 //       Save data from MongoDB into lists
-                        columns.add(key);
-                        values.add(value != null ? value.toString():null);
+//                        columns.add(key);
+//                        values.add(value != null ? value.toString():null);
+                        docData.put(key,value);
 
                         System.out.println(key + " & the value: " + value);
                     }
 
 //       convert cols to string like:[_id,name,..] to ("_id,name,.."), to be valid for SQL statement
-                    String colStr = String.join(", ",columns);
-//       convert vals space to (?,?,..), to be valid for SQL statement
-                    String valStr = String.join(", ", Collections.nCopies(columns.size(), "?"));
+//                    String colStr = String.join(", ",columns);
+                    String colStr = String.join(", ",docData.keySet());
+
+
+                //       convert vals space to (?,?,..), to be valid for SQL statement
+                    String valStr = String.join(", ", Collections.nCopies(docData.keySet().size(), "?"));
                     sql = "INSERT INTO "+c  +"(" + colStr + ") values ("+ valStr +")";
 
 
                     try (PreparedStatement psmt = psql.conn.prepareStatement(sql)){
 //       insert values into SQL statement
-                        for (int i = 0; i < values.size(); i++) {
-                            String value = values.get(i);
-                            Object originalValue = doc.get(columns.get(i));
+//                        for (int i = 0; i < docData.keySet().size(); i++) {
+//                            String value = values.get(i);
+//                            Object originalValue = doc.get(columns.get(i));
+//
+//                            if (value == null) {
+//                                psmt.setNull(i + 1, java.sql.Types.NULL);
+//                            } else if (originalValue instanceof Integer) {
+//                                psmt.setInt(i + 1, (Integer) originalValue);
+//                            } else if (originalValue instanceof Long) {
+//                                psmt.setLong(i + 1, (Long) originalValue);
+//                            } else if (originalValue instanceof Double) {
+//                                psmt.setDouble(i + 1, (Double) originalValue);
+//                            } else if (originalValue instanceof Boolean) {
+//                                psmt.setBoolean(i + 1, (Boolean) originalValue);
+//                            } else if (originalValue instanceof Date) {
+//                                psmt.setTimestamp(i + 1, new java.sql.Timestamp(((Date) originalValue).getTime()));
+//                            } else {
+//                                psmt.setString(i + 1, value);
+//                            }
+//                        }
+                        int i = 0;
+                        for (String key:docData.keySet()) {
+                            Object originalValue = docData.get(key);
+//                            Object originalValue = doc.get(columns.get(i));
 
-                            if (value == null) {
+                            if (originalValue == null) {
                                 psmt.setNull(i + 1, java.sql.Types.NULL);
                             } else if (originalValue instanceof Integer) {
                                 psmt.setInt(i + 1, (Integer) originalValue);
@@ -77,8 +103,9 @@ public class App
                             } else if (originalValue instanceof Date) {
                                 psmt.setTimestamp(i + 1, new java.sql.Timestamp(((Date) originalValue).getTime()));
                             } else {
-                                psmt.setString(i + 1, value);
+                                psmt.setString(i + 1, originalValue.toString());
                             }
+                             i +=1;
                         }
                         psmt.executeUpdate();
                         System.out.println("Inserted successfully!");
@@ -86,22 +113,13 @@ public class App
                     }catch (Exception e){
                         System.out.println("Inserted FFFFFFFFFFFf"+e.getMessage());
                     }
-//                }
 
 
-////        Retrieve key & values in each doc , and store in HashMap
-//                for(String key:doc.keySet()){
-//                    String value = doc.get(key).toString();
-//                    System.out.println(key + " & the value: "+ value);
-//                    hashMap.put(key,value);
-//                }
 
 
             }
             System.out.println("--------------------------");
         }
-
-//        System.out.println(hashMap);
 
 
 
