893059d8df280b5e87708bdb85798bb1ca941a41
nada969 <nadamousa969@gmail.com>
2025-11-25 17:28:26 +0200

    update ReadJSON

--- src/main/java/org/example/ReadJSON.java
+++ src/main/java/org/example/ReadJSON.java
@@ -2,8 +2,13 @@ package org.example;
 
 //if JSON has somthing not found in java file: ignore it and dont throww error
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-@JsonIgnoreProperties(ignoreUnknown = true)
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReadJSON {
     static class Mapping {
         String mongoKey;
@@ -30,56 +35,56 @@ public class ReadJSON {
     Mapping[] mappings ;
 
 
-
-    public String  getSource(){
-        return source;
-    }
-    public void setSource(String source){
-        this.source = source;
-    }
-    public String getDestination(){
-        return destination;
-    }
-    public void setDestination(String destination){
-        this.destination = destination;
-    }
-    public String getUrlDestination(){
-        return urlDestination;
-    }
-    public void setUrlDestination(String urlDestination){
-        this.urlDestination = urlDestination;
-    }
-    public String getDestinationPassWord(){
-        return destinationPassWord;
-    }
-    public void setDestinationPassWord(String destinationPassWord){
-        this.destinationPassWord = destinationPassWord;
-    }
-    public String  getCollectionName(){
-        return collectionName;
-    }
-    public void setCollectionName(String collectionName){
-        this.collectionName = collectionName;
-    }
-
-    public String getTableName(){
-        return tableName;
-    }
-    public void setTableName(String tableName){
-        this.tableName = tableName;
-    }
-
-    public String getCronSchedule(){
-        return cronSchedule;
-    }
-    public void setCronSchedule(String cronSchedule){
-        this.cronSchedule = cronSchedule;
-    }
-
-    public Mapping[] getMappings() {
-        return mappings;
-    }
-    public void setMappings(Mapping[] mappings) {
-        this.mappings = mappings;
-    }
+//
+//    public String  getSource(){
+//        return source;
+//    }
+//    public void setSource(String source){
+//        this.source = source;
+//    }
+//    public String getDestination(){
+//        return destination;
+//    }
+//    public void setDestination(String destination){
+//        this.destination = destination;
+//    }
+//    public String getUrlDestination(){
+//        return urlDestination;
+//    }
+//    public void setUrlDestination(String urlDestination){
+//        this.urlDestination = urlDestination;
+//    }
+//    public String getDestinationPassWord(){
+//        return destinationPassWord;
+//    }
+//    public void setDestinationPassWord(String destinationPassWord){
+//        this.destinationPassWord = destinationPassWord;
+//    }
+//    public String  getCollectionName(){
+//        return collectionName;
+//    }
+//    public void setCollectionName(String collectionName){
+//        this.collectionName = collectionName;
+//    }
+//
+//    public String getTableName(){
+//        return tableName;
+//    }
+//    public void setTableName(String tableName){
+//        this.tableName = tableName;
+//    }
+//
+//    public String getCronSchedule(){
+//        return cronSchedule;
+//    }
+//    public void setCronSchedule(String cronSchedule){
+//        this.cronSchedule = cronSchedule;
+//    }
+//
+//    public Mapping[] getMappings() {
+//        return mappings;
+//    }
+//    public void setMappings(Mapping[] mappings) {
+//        this.mappings = mappings;
+//    }
 }
