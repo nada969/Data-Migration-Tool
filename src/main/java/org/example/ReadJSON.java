a6919f1c534e34d155490e54545f7c2613fc0f66
nada969 <nadamousa969@gmail.com>
2025-11-25 17:50:01 +0200

    update ReadJSON

--- src/main/java/org/example/ReadJSON.java
+++ src/main/java/org/example/ReadJSON.java
@@ -4,18 +4,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
+
+
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReadJSON {
 
-    @Getter
-    @Setter
-    public static class Mapping {
-        private String mongoKey;
-        private String psqlCol;
-        private String type;
-    }
+//    @Getter
+//    @Setter
+//    public static class Mapping {
+//        private String mongoKey;
+//        private String psqlCol;
+//        private String type;
+//    }
+
+//  using record : because this class only carry vars....
+    record Mapping(String mongoKey, String psqlCol ,String type) { };
 
     private String source;
     private String destination;
