944329b56ee2ff2ef83ef23533d4b144949c2f37
nada969 <nadamousa969@gmail.com>
2025-11-25 22:53:22 +0200

    update ReadJSON

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -46,6 +46,7 @@ public class App2 {
         String collectionName = readJSON.getCollectionName();
         MongoCollection<Document> collection = db.getCollection(collectionName);
 
+
 //        loop for each doc in the collection:  Extract data from MongoCollection & put them in HashTable
         for(Document doc:collection.find()){
 
@@ -73,56 +74,57 @@ public class App2 {
 
                 PreparedStatement psmt = psql.conn.prepareStatement(sql);
                 int i = 0;
-                for (String key : docData.keySet()) {
-                    Object originalValue = docData.get(key);
-
-                    if (originalValue == null) {
-                        psmt.setNull(i + 1, java.sql.Types.NULL);
-                    } else if (originalValue instanceof Integer) {
-                        psmt.setInt(i + 1, (Integer) originalValue);
-                    } else if (originalValue instanceof Long) {
-                        psmt.setLong(i + 1, (Long) originalValue);
-                    } else if (originalValue instanceof Double) {
-                        psmt.setDouble(i + 1, (Double) originalValue);
-                    } else if (originalValue instanceof Boolean) {
-                        psmt.setBoolean(i + 1, (Boolean) originalValue);
-                    } else if (originalValue instanceof Date) {
-                        psmt.setTimestamp(i + 1, new java.sql.Timestamp(((Date) originalValue).getTime()));
-                    } else {
-                        psmt.setString(i + 1, originalValue.toString());
-                    }
-                    i += 1;
-                }
+//                for (String key : docData.keySet()) {
+//                    Object originalValue = docData.get(key);
+//
+//                    if (originalValue == null) {
+//                        psmt.setNull(i + 1, java.sql.Types.NULL);
+//                    } else if (originalValue instanceof Integer) {
+//                        psmt.setInt(i + 1, (Integer) originalValue);
+//                    } else if (originalValue instanceof Long) {
+//                        psmt.setLong(i + 1, (Long) originalValue);
+//                    } else if (originalValue instanceof Double) {
+//                        psmt.setDouble(i + 1, (Double) originalValue);
+//                    } else if (originalValue instanceof Boolean) {
+//                        psmt.setBoolean(i + 1, (Boolean) originalValue);
+//                    } else if (originalValue instanceof Date) {
+//                        psmt.setTimestamp(i + 1, new java.sql.Timestamp(((Date) originalValue).getTime()));
+//                    } else {
+//                        psmt.setString(i + 1, originalValue.toString());
+//                    }
+//                    i += 1;
+//                }
                 psmt.executeUpdate();
             }
             catch (Exception e){
 //need to fix
-                PreparedStatement psmt2 = psql.conn.prepareStatement(sql2);
-                psmt2.executeQuery();
-
-                PreparedStatement psmt = psql.conn.prepareStatement(sql);
-                int i = 0;
-                for (String key : docData.keySet()) {
-                    Object originalValue = docData.get(key);
-
-                    if (originalValue == null) {
-                        psmt.setNull(i + 1, java.sql.Types.NULL);
-                    } else if (originalValue instanceof Integer) {
-                        psmt.setInt(i + 1, (Integer) originalValue);
-                    } else if (originalValue instanceof Long) {
-                        psmt.setLong(i + 1, (Long) originalValue);
-                    } else if (originalValue instanceof Double) {
-                        psmt.setDouble(i + 1, (Double) originalValue);
-                    } else if (originalValue instanceof Boolean) {
-                        psmt.setBoolean(i + 1, (Boolean) originalValue);
-                    } else if (originalValue instanceof Date) {
-                        psmt.setTimestamp(i + 1, new java.sql.Timestamp(((Date) originalValue).getTime()));
-                    } else {
-                        psmt.setString(i + 1, originalValue.toString());
-                    }
-                    i += 1;
-                }
-                psmt.executeUpdate();
+//                PreparedStatement psmt2 = psql.conn.prepareStatement(sql2);
+//                psmt2.executeQuery();
+//
+//                PreparedStatement psmt = psql.conn.prepareStatement(sql);
+//                int i = 0;
+//                for (String key : docData.keySet()) {
+//                    Object originalValue = docData.get(key);
+//
+//
+//                    if (originalValue == null) {
+//                        psmt.setNull(i + 1, java.sql.Types.NULL);
+//                    } else if (originalValue instanceof Integer) {
+//                        psmt.setInt(i + 1, (Integer) originalValue);
+//                    } else if (originalValue instanceof Long) {
+//                        psmt.setLong(i + 1, (Long) originalValue);
+//                    } else if (originalValue instanceof Double) {
+//                        psmt.setDouble(i + 1, (Double) originalValue);
+//                    } else if (originalValue instanceof Boolean) {
+//                        psmt.setBoolean(i + 1, (Boolean) originalValue);
+//                    } else if (originalValue instanceof Date) {
+//                        psmt.setTimestamp(i + 1, new java.sql.Timestamp(((Date) originalValue).getTime()));
+//                    } else {
+//                        psmt.setString(i + 1, originalValue.toString());
+//                    }
+//                    i += 1;
+//                }
+//                psmt.executeUpdate();
 
                 System.out.println("Inserted FFFFFFFFFFFf"+e.getMessage());
             }
